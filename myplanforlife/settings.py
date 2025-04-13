033fe7dabcd2330d790f5be28d8bff23633d4a37
kev613 <kjwagner613@gmail.com>
2025-04-13 14:54:39 -0700

    prepping another try

--- myplanforlife/settings.py
+++ myplanforlife/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
+from dotenv import load_dotenv
+import os
+
+import dj_database_url
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOGIN_REDIRECT_URL = '/'
@@ -21,12 +29,13 @@ LOGIN_REDIRECT_URL = '/'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3t)2eil@)y9w04earl2-%wcl!k^dnxpnw&q%j#0v4!&8*u95r3'
+SECRET_KEY = os.getenv('FIPqfOB1vfTRhcZb77t7S-P5f9EzTCz9nQ22SQU1-lFsFrTus-QqQyUn8XwWiwUy05k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+if not 'ON_HEROKU' in os.environ:
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,16 +84,23 @@ WSGI_APPLICATION = 'myplanforlife.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myplanforlife',
-        'USER': 'kjwagner613',
-        'PASSWORD': 'devpass123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'ON_HEROKU' in os.environ:
+    DATABASES = {
+        "default": dj_database_url.config(
+            env='DATABASE_URL',
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': '<myplanforlife>',
+            # The value of 'NAME' should match the value of 'NAME' you replaced.
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -119,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
