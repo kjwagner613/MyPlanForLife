3741a2957bdd0bfb3b4e9ff225c603b38586f935
kev613 <kjwagner613@gmail.com>
2025-04-15 01:20:47 -0700

    updating hosts list

--- myplanforlife/settings.py
+++ myplanforlife/settings.py
@@ -24,10 +24,7 @@ SECRET_KEY = 'FIPqfOB1vfTRhcZb77t7S-P5f9EzTCz9nQ22SQU1-lFsFrTus-QqQyUn8XwWiwUy05
 if not 'ON_HEROKU' in os.environ:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', myplanforlife-05b1598c3053.herokuapp.com']
-
-
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'myplanforlife-05b1598c3053.herokuapp.com']
 
 
 
